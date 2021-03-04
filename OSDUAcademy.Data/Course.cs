d958f27afcc39cd01caf304f0265e057ea31a366
Alan Rostem <arostem@gmail.com>
2021-03-04 20:58:07 +0100

    Added project files for the API

---
+++ OSDUAcademy.Data/Course.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OSDUAcademy.Data
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public byte Rating { get; set; }
+    }
+}
