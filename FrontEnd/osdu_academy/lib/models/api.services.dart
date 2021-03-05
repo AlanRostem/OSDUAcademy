8841354d13ce4536ab7f75860b73417ab9c5aff6
Alan Rostem <arostem@gmail.com>
2021-03-05 16:34:03 +0100

    Restarting project files again

--- FrontEnd/osdu_academy/lib/models/api.services.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:convert' as convert;
-import 'package:http/http.dart' as http;
-
-class ApiServices {
-  static String courseUrl = "https://localhost:44303/api/course";
-  static Future fetchCourse() async {
-    return await http.get(courseUrl);
-  }
-}
