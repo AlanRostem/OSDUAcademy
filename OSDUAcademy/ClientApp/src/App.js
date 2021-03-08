d505e83b94a6a01f14429b5d10edbe58be921c41
Alan Rostem <arostem@gmail.com>
2021-03-08 16:40:32 +0100

    Update App.js

--- OSDUAcademy/ClientApp/src/App.js
+++ OSDUAcademy/ClientApp/src/App.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { DefaultLayout } from './components/DefaultLayout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import React, {Component} from 'react';
+import {Route} from 'react-router';
+import {DefaultLayout} from './components/DefaultLayout';
+import {Home} from './components/Home';
+import {FetchData} from './components/FetchData';
+import {Counter} from './components/Counter';
 
 import './custom.css'
 
 export default class App extends Component {
-  static displayName = App.name;
+    static displayName = App.name;
 
-  render () {
-    return (
-        <AppContainer>
-          <DefaultLayout>
-            <Route exact path='/' component={Home} />
-            <Route path='/counter' component={Counter} />
-            <Route path='/fetch-data' component={FetchData} />
-          </DefaultLayout>
-        </AppContainer>
-    );
-  }
+    render() {
+        return (
+            <DefaultLayout>
+                <Route exact path='/' component={Home}/>
+                <Route path='/counter' component={Counter}/>
+                <Route path='/fetch-data' component={FetchData}/>
+            </DefaultLayout>
+        );
+    }
 }
