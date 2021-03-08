23c8bdbb46d079c91e5524b7071c7edae60578b6
Alan Rostem <arostem@gmail.com>
2021-03-08 16:39:36 +0100

    Revert "Turned it back to normal"
    This reverts commit 603692a1754261d4a599e210c69cdfac62173b02.

--- OSDUAcademy/ClientApp/src/App.js
+++ OSDUAcademy/ClientApp/src/App.js
@@ -1,22 +1,24 @@
-import React, {Component} from 'react';
-import {Route} from 'react-router';
-import {DefaultLayout} from './components/DefaultLayout';
-import {Home} from './components/Home';
-import {FetchData} from './components/FetchData';
-import {Counter} from './components/Counter';
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+import { DefaultLayout } from './components/DefaultLayout';
+import { Home } from './components/Home';
+import { FetchData } from './components/FetchData';
+import { Counter } from './components/Counter';
 
 import './custom.css'
 
 export default class App extends Component {
-    static displayName = App.name;
+  static displayName = App.name;
 
-    render() {
-        return (
-            <DefaultLayout>
-                <Route exact path='/' component={Home}/>
-                <Route path='/counter' component={Counter}/>
-                <Route path='/fetch-data' component={FetchData}/>
-            </DefaultLayout>
-        );
-    }
+  render () {
+    return (
+        <AppContainer>
+          <DefaultLayout>
+            <Route exact path='/' component={Home} />
+            <Route path='/counter' component={Counter} />
+            <Route path='/fetch-data' component={FetchData} />
+          </DefaultLayout>
+        </AppContainer>
+    );
+  }
 }
