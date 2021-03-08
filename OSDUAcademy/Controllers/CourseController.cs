455579985dcbe0a05c9ca691c7e9a47d7010f81d
Alan Rostem <arostem@gmail.com>
2021-03-08 10:38:15 +0100

    Can now fetch from the database

--- OSDUAcademy/Controllers/CourseController.cs
+++ OSDUAcademy/Controllers/CourseController.cs
@@ -2,37 +2,40 @@
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
 using MongoDB;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using OSDUAcademy.DataTypes;
 
 namespace OSDUAcademy.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class CourseController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        private readonly ILogger<CourseController> _logger;
+        private static readonly string DbConnectionStr = "mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb";
+        
+        public CourseController(ILogger<CourseController> logger)
         {
             _logger = logger;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<Course> Get()
         {
-            var client = new MongoClient("mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb");
+            var client = new MongoClient(DbConnectionStr);
             var database = client.GetDatabase("osdu_academy");
-            var collection = database.GetCollection<BsonDocument>("courses");
-            return null;
+            var collection = database.GetCollection<Course>("courses");
+            return collection.Find(s=> true /*s.AvgRating > 3f*/).ToList();
         }
     }
 }
---
+++ OSDUAcademy/DataTypes/Course.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace OSDUAcademy.DataTypes
+{
+    [BsonIgnoreExtraElements]
+    public class Course
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        
+        [BsonElement("title")]
+        public string Title { get; set; }
+        
+        [BsonElement("desc")]
+        public string Description { get; set; }
+
+        [BsonElement("avg_rating")]
+        public double AvgRating { get; set; }
+    }
+}
