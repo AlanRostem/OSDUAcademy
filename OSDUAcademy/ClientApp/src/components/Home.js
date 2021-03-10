00afe751f9ebe1528f98eb0fd1ef27d0e89d7b0f
Alan Rostem <arostem@gmail.com>
2021-03-10 14:19:05 +0100

    Added background image to the banner

--- OSDUAcademy/ClientApp/src/components/Home.js
+++ OSDUAcademy/ClientApp/src/components/Home.js
@@ -11,7 +11,7 @@ export class Home extends Component {
             <div>
                 <DefaultNavMenu/>
                 <Container>
-                    <div className="banner">
+                    <div className="banner" style={{backgroundImage: "url(" + process.env.PUBLIC_URL + "img/oil-rig.png"}}>
                         <h1>Grow your career skills</h1>
                         <p>Explore a world of courses at OSDU Academy to find your perfect suit free of charge</p>
 
