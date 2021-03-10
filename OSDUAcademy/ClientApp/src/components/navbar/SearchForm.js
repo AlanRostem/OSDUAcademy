7e1debab6396cb439525b8c28be93674e8a9a7fd
Alan Rostem <arostem@gmail.com>
2021-03-10 11:42:54 +0100

    Added a search form and installed boostrap for react

---
+++ OSDUAcademy/ClientApp/src/components/navbar/SearchForm.js
@@ -0,0 +1,35 @@
+﻿import React, {Component} from "react"
+
+export default class SearchForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ""};
+        
+        this.inputFieldStyle = {};
+        this.inputFieldStyle.minHeight = "2.2rem";
+        this.inputFieldStyle.width = "300px";
+    }
+    
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+    
+    handleSubmit(event) {
+        console.log("You searched for", this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit.bind(this)}>
+                <input 
+                    type="text" 
+                    className="fa fa-search" 
+                    value={this.state.value}
+                    placeholder="Search"
+                    onChange={this.handleChange.bind(this)} 
+                    style={this.inputFieldStyle} />
+            </form>
+        );
+    }
+}
