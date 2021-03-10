c5f0948cf540cb1d03e78b7ae7f7b547c0543232
Alan Rostem <arostem@gmail.com>
2021-03-10 17:41:34 +0100

    Fixed the course card link type

--- OSDUAcademy/ClientApp/src/components/home/CourseCard.js
+++ OSDUAcademy/ClientApp/src/components/home/CourseCard.js
@@ -1,5 +1,6 @@
 ﻿import React, { Component } from "react"
-import {Col} from "reactstrap";
+import {Link, NavLink} from "react-router-dom";
+import {NavItem} from "reactstrap";
 
 export default class CourseCard extends Component {
     render() {
@@ -12,12 +13,12 @@ export default class CourseCard extends Component {
             title += "...";
         }
         return (
-            <div className="course-card">
+            <NavLink tag={Link} className="course-card" to="/">
                 <img style={{width: "240px", height: "135px"}} src={process.env.PUBLIC_URL + "/" + this.props.imgSrc} alt={"Course: " + this.props.title}/>
                 <h6>{title}</h6>
                 <p className="dim-text">{this.props.desc}</p>
                 <p><span className="difficulty-tag">{this.props.difficulty}</span><span className="domain-tag">{this.props.domain}</span></p>
-            </div>
+            </NavLink>
         );
     }
 }
