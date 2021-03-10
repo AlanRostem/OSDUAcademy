ad44d59baa94863d82edd9b9dfab9a7e91d89e7a
Alan Rostem <arostem@gmail.com>
2021-03-10 12:47:49 +0100

    Added a cute little search bar

--- OSDUAcademy/ClientApp/src/components/navbar/SearchForm.js
+++ OSDUAcademy/ClientApp/src/components/navbar/SearchForm.js
@@ -5,33 +5,52 @@ export default class SearchForm extends Component {
     constructor(props) {
         super(props);
         this.state = {value: ""};
-        
+
         this.inputFieldStyle = {};
         this.inputFieldStyle.minHeight = "2.2rem";
         this.inputFieldStyle.width = "300px";
+        this.inputFieldStyle.marginLeft = "1.5rem";
     }
-    
+
     handleChange(event) {
         this.setState({value: event.target.value});
     }
-    
+
     handleSubmit(event) {
-        console.log("You searched for", this.state.value);
         event.preventDefault();
+        if (this.state.value.length === 0) return;
+        console.log("You searched for", this.state.value);
     }
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit.bind(this)}>
-                <input 
-                    type="text" 
-                    className="fa fa-search" 
+            <form className="input-group" style={this.inputFieldStyle} onSubmit={this.handleSubmit.bind(this)}>
+                <input
+                    className="form-control"
+                    onChange={this.handleChange.bind(this)}
                     value={this.state.value}
+                    type="text"
                     placeholder="Search"
-                    onChange={this.handleChange.bind(this)} 
-                    style={this.inputFieldStyle} 
                 />
+                <div className="input-group-append">
+                    <button className="btn btn-secondary" type="submit">
+                        <i className="fa fa-search"/>
+                    </button>
+                </div>
             </form>
         );
     }
 }
+
+/*
+    <span className="fa fa-search" />
+                <input
+                    onChange={this.handleChange.bind(this)}
+                    style={this.inputFieldStyle}
+                    value={this.state.value}
+                    type="text"
+                    placeholder="Search"
+                    className="form-control"
+                />
+
+*/
