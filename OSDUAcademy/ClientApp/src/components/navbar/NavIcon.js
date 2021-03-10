007de2ba4d77d64399fbea3db2441a25cd7bc987
Alan Rostem <arostem@gmail.com>
2021-03-10 14:22:50 +0100

    Changed navicon sizes

--- OSDUAcademy/ClientApp/src/components/navbar/NavIcon.js
+++ OSDUAcademy/ClientApp/src/components/navbar/NavIcon.js
@@ -4,7 +4,7 @@ export default class NavIcon extends Component {
     render() {
         return (
             <div className="text-center" >
-                <i className={this.props.iconClass + " fa-2x"}/>
+                <i className={this.props.iconClass + " fa-lg"}/>
                 <br/>
                 <span>{this.props.text}</span>
             </div>
