2e3421c9108d076d70fd913dab3cafe85e995f7e
Alan Rostem <arostem@gmail.com>
2021-03-12 19:54:40 +0100

    Using a library to slide the courses

--- OSDUAcademy/ClientApp/src/components/home/CourseRow.js
+++ OSDUAcademy/ClientApp/src/components/home/CourseRow.js
@@ -1,13 +1,16 @@
 ﻿import React, {Component} from "react"
 import CourseScrollBar from "./CourseScrollBar";
 import shortid from "shortid";
+import {CarouselProvider, Slider, Slide, ButtonBack, ButtonNext} from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+import CourseCard from "./CourseCard";
 
 export default class CourseRow extends Component {
     selfId = shortid.generate();
     rowId = shortid.generate();
-    
+
     // currentScrollAmount = 0;
-    
+
     constructor(props) {
         super(props);
         this.state = {mounted: false, containerWidth: undefined};
@@ -24,7 +27,7 @@ export default class CourseRow extends Component {
         // let row = document.getElementById(this.getRowId());
         // row.style.left = (this.currentScrollAmount -= direction * this.state.containerWidth) + "px";
     }
-    
+
     render() {
         let scrollBar = this.state.mounted ?
             <CourseScrollBar
@@ -35,11 +38,33 @@ export default class CourseRow extends Component {
 
         return (
             <div id={this.selfId}>
-                <div id={this.rowId} className="course-row">
-                    {this.props.children}
-                </div>
-                {scrollBar}
+                <CarouselProvider
+                    naturalSlideWidth={100}
+                    naturalSlideHeight={120}
+                    visibleSlides={4}
+                    totalSlides={this.props.children.length}>
+                    <Slider>
+                        {
+                            this.props.children.map((child, i) => {
+                                return (
+                                    <Slide index={i}>
+                                        {child}
+                                    </Slide>
+                                );
+                            })
+                        }
+                    </Slider>
+                    <ButtonBack>Back</ButtonBack>
+                    <ButtonNext>Next</ButtonNext>
+                </CarouselProvider>
             </div>
         );
     }
 }
+
+/*    
+<div id={this.rowId} className="course-row">
+    {this.props.children}
+</div>               
+{scrollBar}
+ */
