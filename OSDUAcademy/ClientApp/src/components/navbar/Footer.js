66dbf33b22c7d268b619102275fdab5a3f050f55
Alan Rostem <arostem@gmail.com>
2021-03-18 13:08:26 +0100

    Fixed some more warnings

--- OSDUAcademy/ClientApp/src/components/navbar/Footer.js
+++ OSDUAcademy/ClientApp/src/components/navbar/Footer.js
@@ -9,10 +9,10 @@ export class Footer extends Component {
             <footer>
                 
                 <ul className="footer-row">
-                    <li className="footer-col"><a href="#">Home</a></li>
-                    <li className="footer-col"><a href="#">About</a></li>
+                    <li className="footer-col"><a href="/">Home</a></li>
+                    <li className="footer-col"><a href="/">About</a></li>
                     <li className="footer-col"><a href="/course-interface">Services</a></li>
-                    <li className="footer-col"><a href="#">Contact</a></li>
+                    <li className="footer-col"><a href="/">Contact</a></li>
                 </ul>
                 <hr style={{backgroundColor:"white", width:"50%"}}/>
                 <p className="footer-lic">OSDU Academy © 2021 | By Schlumberger </p>
