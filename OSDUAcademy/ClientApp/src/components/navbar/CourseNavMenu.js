859f2ab1fd8f43483033c6d24cfafa0d3a74db00
Nemanja Babic <nemanja0512@gmail.com>
2021-03-18 14:24:49 +0100

    changed course interface design

--- OSDUAcademy/ClientApp/src/components/navbar/CourseNavMenu.js
+++ OSDUAcademy/ClientApp/src/components/navbar/CourseNavMenu.js
@@ -1,9 +1,7 @@
 ﻿import React, {Component} from 'react';
-import {Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink} from 'reactstrap';
+import {Collapse, Navbar, NavItem, NavLink} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import './NavMenu.css';
-import SchlumbergerLogo from "./SchlumbergerLogo"
-import VerticalDivider from "./VerticalDivider";
 import NavIcon from "./NavIcon";
 import {CourseSideBar} from "../course-interface/CourseSideBar";
 import {ChapterDrop} from "../chapterdrop/ChapterDrop";
@@ -43,54 +41,51 @@ export class CourseNavMenu extends Component {
         return (
             <header>
                 <Navbar
-                    className="navbar-expand-sm navbar-toggleable-sm ng-white box-shadow navbar-slb"
+                    className="navbar-expand-sm navbar-toggleable-sm ng-white box-shadow navbar-slb navbar-width"
                     light>
-
                     
-                        <NavbarBrand tag={Link} to="/"><SchlumbergerLogo/></NavbarBrand>
-                        <NavbarBrand><VerticalDivider/></NavbarBrand>
-                        <NavbarBrand tag={Link} to="/" style={{fontWeight: "bold", color: "white", fontSize: "1.5rem"}}>OSDU
-                            Academy</NavbarBrand>
-
-                        <NavbarToggler onClick={this.toggleNavbar} className="mr-2"/>
-                        <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed}
-                                  navbar>
-                            <ul className="navbar-nav flex-grow">
-                                <NavItem>
-                                    <NavLink tag={Link} to="/" className="text-light overview-button" onClick={this.showOverview.bind(this)}>
-                                        <NavIcon text="OVERVIEW" iconClass="fa fa-bars"/>
-                                    </NavLink>
-                                    {
-                                        this.state.showOverview
-                                            ? (
-                                                <CourseSideBar onClose={this.showOverview.bind(this)}
-                                                >
-                                                    <ChapterDrop name="Introduction" amount="1">
-                                                        <ChapterItem subchapter="What you will learn"/>
-                                                    </ChapterDrop>
-                                                    <ChapterDrop name="Data Management" amount="5">
-                                                        <ChapterItem subchapter="Somebody once told me"/>
-                                                        <ChapterItem subchapter="The worlds gonna roll me"/>
-                                                        <ChapterItem subchapter="I ain't the sharpest tool in the shed"/>
-                                                        <ChapterItem subchapter="She was looking kind of dumb with her finger and her thumb She was looking kind of dumb with her finger and her thumb She was looking kind of dumb with her finger and her thumb She was looking kind of dumb with her finger and her thumb"/>
-                                                        <ChapterItem subchapter="In the shape of an L on her forehead"/>
-                                                    </ChapterDrop>
-                                                    <ChapterDrop name="Another thing" amount="4">
-                                                        <ChapterItem subchapter="Small things"/>
-                                                        <ChapterItem subchapter="Bigger things"/>
-                                                        <ChapterItem subchapter="Even bigger stuff"/>
-                                                        <ChapterItem subchapter="The absolute biggest"/>
-                                                    </ChapterDrop>
-                                                    <ChapterDrop name="The last thing" amount="1">
-                                                        <ChapterItem subchapter="Summary"/>
-                                                    </ChapterDrop>
-                                                </CourseSideBar>
-                                            )
-                                            : null
-                                    }
-                                </NavItem>
-                            </ul>
-                        </Collapse>
+                    <div>
+                        <NavLink tag={Link} to="/" className="text-light home-button">
+                            <NavIcon text="COURSE" iconClass="fa fa-book"/>
+                        </NavLink>
+                    </div>
+                    <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed}
+                              navbar>
+                        <ul className="navbar-nav flex-grow">
+                            <NavItem>
+                                <NavLink tag={Link} to="/course-front-page/:courseRoute" className="text-light overview-button" onClick={this.showOverview.bind(this)}>
+                                    <NavIcon text="OVERVIEW" iconClass="fa fa-bars"/>
+                                </NavLink>
+                                {
+                                    this.state.showOverview
+                                        ? (
+                                            <CourseSideBar onClose={this.showOverview.bind(this)}>
+                                                <ChapterDrop name="Introduction" amount="1">
+                                                    <ChapterItem subchapter="What you will learn"/>
+                                                </ChapterDrop>
+                                                <ChapterDrop name="Data Management" amount="5">
+                                                    <ChapterItem subchapter="Somebody once told me"/>
+                                                    <ChapterItem subchapter="The worlds gonna roll me"/>
+                                                    <ChapterItem subchapter="I ain't the sharpest tool in the shed"/>
+                                                    <ChapterItem subchapter="She was looking kind of dumb with her finger and her thumb She was looking kind of dumb with her finger and her thumb She was looking kind of dumb with her finger and her thumb She was looking kind of dumb with her finger and her thumb"/>
+                                                    <ChapterItem subchapter="In the shape of an L on her forehead"/>
+                                                </ChapterDrop>
+                                                <ChapterDrop name="Another thing" amount="4">
+                                                    <ChapterItem subchapter="Small things"/>
+                                                    <ChapterItem subchapter="Bigger things"/>
+                                                    <ChapterItem subchapter="Even bigger stuff"/>
+                                                    <ChapterItem subchapter="The absolute biggest"/>
+                                                </ChapterDrop>
+                                                <ChapterDrop name="The last thing" amount="1">
+                                                    <ChapterItem subchapter="Summary"/>
+                                                </ChapterDrop>
+                                            </CourseSideBar>
+                                        )
+                                        : null
+                                }
+                            </NavItem>
+                        </ul>
+                    </Collapse>
                     
 
                 </Navbar>
