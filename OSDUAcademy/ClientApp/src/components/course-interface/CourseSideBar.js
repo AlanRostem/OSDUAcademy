6bb579d27fcfaa405fd5c9c18af78a2e76850545
Alan Rostem <arostem@gmail.com>
2021-03-18 12:52:09 +0100

    Update CourseSideBar.js

--- OSDUAcademy/ClientApp/src/components/course-interface/CourseSideBar.js
+++ OSDUAcademy/ClientApp/src/components/course-interface/CourseSideBar.js
@@ -3,21 +3,28 @@ import React, {Component} from 'react';
 export class CourseSideBar extends Component {
     static displayName = CourseSideBar.name;
     state = {closed: false}
+
     close() {
         this.setState({closed: true})
     }
-    
+
     render() {
         return (
             <div className="course-sidebar">
-                <div className="overview-h">
-                    <h3 className="slightly-dim">Course content <button className="fa fa-times close-btn" aria-hidden="true" onClick={this.close.bind(this)}/></h3>
-                </div>
-                
                 {
-                    !this.state.closed ? 
-                        
-                    this.props.children : undefined
+                    !this.state.closed ?
+                        <div>
+                            <div className="overview-h">
+                                <h3 className="slightly-dim">Course content <button className="fa fa-times close-btn"
+                                                                                    aria-hidden="true"
+                                                                                    onClick={this.close.bind(this)}/>
+                                </h3>
+                            </div>
+                            {
+                                this.props.children
+                            }
+                        </div>
+                        : undefined
                 }
             </div>
         );
