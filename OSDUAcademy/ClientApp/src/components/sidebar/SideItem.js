79b2e3d47e8cf62b7f8effb4554459781190bd9b
Nemanja Babic <nemanja0512@gmail.com>
2021-03-18 13:08:52 +0100

    unused components deleted

--- OSDUAcademy/ClientApp/src/components/sidebar/SideItem.js
+++ OSDUAcademy/ClientApp/src/components/sidebar/SideItem.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
-import { Link } from 'react-router-dom';
 
 export class SideItem extends Component {
     static displayName = SideItem.name;
