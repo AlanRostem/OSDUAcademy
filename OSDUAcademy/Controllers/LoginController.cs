9288a6675cd93025e2780bbccb2eb979c4824f6d
Alan Rostem <arostem@gmail.com>
2021-03-19 13:39:38 +0100

    Login mechanicsm implemented

--- OSDUAcademy/Controllers/LoginController.cs
+++ OSDUAcademy/Controllers/LoginController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
 using Newtonsoft.Json.Linq;
+using OSDUAcademy.DataTypes;
 
 namespace OSDUAcademy.Controllers
 {
@@ -12,17 +15,46 @@ namespace OSDUAcademy.Controllers
     {
         public class LoginData
         {
-            [Required]
-            public string Email { get; set; }
-    
-            [Required]
-            public string Password { get; set; }
+            [Required] public string Email { get; set; }
+
+            [Required] public string Password { get; set; }
         }
-        
-        [HttpPost]
-        public bool Post([FromBody] LoginData content)
+
+        private static readonly ProjectionDefinitionBuilder<User> UserFieldBuilder = Builders<User>.Projection;
+
+        private readonly IMongoCollection<User> _userCollection;
+
+        public LoginController(IMongoClient client)
         {
-            return false;
+            var database = client.GetDatabase("osdu_academy");
+            _userCollection = database.GetCollection<User>("users");
+        }
+
+        [HttpPost]
+        public Dictionary<string, object> Post([FromBody] LoginData content)
+        {
+
+            var list = _userCollection
+                .Find(u => u.Email == content.Email)
+                .ToList();
+
+            var data = new Dictionary<string, object>
+            {
+                ["success"] = false
+            };
+
+            if (list.Count != 0)
+            {
+                var user = list[0];
+                if (user.Password == content.Password)
+                {
+                    user.Password = null;
+                    data["success"] = true;
+                    data["user"] = user;
+                }
+            }
+
+            return data;
         }
     }
 }
