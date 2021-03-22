f2b43d4177ac32aa92ae232daf0b6f6c0b52b4f4
Nemanja Babic <nemanja0512@gmail.com>
2021-03-22 14:36:46 +0100

    chapterdrop section documented

--- OSDUAcademy/ClientApp/src/components/chapterdrop/ChapterDrop.js
+++ OSDUAcademy/ClientApp/src/components/chapterdrop/ChapterDrop.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './chapterDrop.css'
 
+/**
+ * The component returns a drop-down menu with different chapters in the course and its functionality.
+ * At the moment, it is used in the "CourseFrontPage" and "CourseNavMenu", but can be used for other drop-down menus in
+ * the app as well. The component includes two properties: "name" (defines header of every menu section) and 
+ * "amount" (the number of subchapters in each section). Both properties are a part of a button that the user can click.
+ */
+
 export class ChapterDrop extends Component {
     static displayName = ChapterDrop.name;
 
     constructor(props) {
         super(props);
 
+        /** The drop bar is closed by default and no data except header and number of subchapters is shown **/
         this.state = {
             dropBar: false,
 
@@ -14,9 +22,10 @@ export class ChapterDrop extends Component {
 
     }
 
-    dropBar(event) {
-        event.preventDefault();
+    /** Function which is called when drop-button is pressed **/
+    dropBar() {
 
+        /** Makes react re-render the component as the state (open/closed) changes **/
         this.setState({
             dropBar: !this.state.dropBar,
         });
