9f4842d0794a8712cabb8e10a5c2511a8b45a5ad
Alan Rostem <arostem@gmail.com>
2021-03-23 18:40:58 +0100

    This is progress

--- OSDUAcademy/ClientApp/src/components/home/CatchUpCard.js
+++ OSDUAcademy/ClientApp/src/components/home/CatchUpCard.js
@@ -1,21 +1,21 @@
-import React, { Component } from "react"
+import React, {Component} from "react"
+import {Link, NavLink} from "react-router-dom";
 
 /**
- * 
+ *
  */
 
 export class CatchUpCard extends Component {
     static displayName = CatchUpCard.name;
-    
+
     render() {
         return (
-            <div className="course-card">
-                <div>
-                    <img src={process.env.PUBLIC_URL + "/thumbnails/courses/" + this.props.routeName + ".png"} alt={"Course: " + this.props.title}/>
-                    <h6>{this.props.title}</h6>
-                    <div style={{textAlign:"center"}}><button className="catch-btn">CONTINUE</button></div>
-                </div>
-            </div>
+            <NavLink tag={Link} className="course-card" to={"/"}>
+                <img src={process.env.PUBLIC_URL + "/thumbnails/courses/" + this.props.routeName + ".png"}
+                     alt={"Course: " + this.props.title}/>
+                <h6>{this.props.title}</h6>
+                <button className="catch-btn">CONTINUE</button>
+            </NavLink>
         );
     }
 }
