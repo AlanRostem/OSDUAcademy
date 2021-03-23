4bb817cd9d5e76f5abdc7fca58ae2a287477e4b1
Alan Rostem <arostem@gmail.com>
2021-03-23 17:15:25 +0100

    Category navmenu should be working

--- OSDUAcademy/ClientApp/src/components/navbar/CategoryNavBar.js
+++ OSDUAcademy/ClientApp/src/components/navbar/CategoryNavBar.js
@@ -1,35 +1,50 @@
 import React, {Component} from 'react';
 import './NavMenu.css';
+import shortid from "shortid"
 
 export class CategoryNavBar extends Component {
     static displayName = CategoryNavBar.name;
+    navBarId = shortid()
+    activeId = ""
 
     populateChildren() {
         return (
             <div>
-                {this.props.children.map((c, i) =>
-                    <div key={i++} style={{display: c.props.itemActive ? "block" : "none"}}>
-                        {c.props.componentToShow}
-                    </div>)}
+                {this.props.children.map((child, i) => {
+                    const id = this.navBarId + i;
+                    let display = "none";
+                    if (child.props.itemActive) {
+                        display = "block";
+                        this.activeId = id;
+                    }
+                    return <div key={i} style={{display: display}} id={id}>
+                        {child.props.componentToShow}
+                    </div>
+                })}
             </div>
         )
     }
 
-    handleTabClick() {
-        console.log("Button: " + 0);
+    handleTabClick(event) {
+        const current = document.getElementById(this.activeId);
+        current.style.display = "none";
+        const next = document.getElementById(event.target.value);
+        next.style.display = "block";
+        this.activeId = event.target.value;
     }
 
     render() {
         return (
             <div>
                 <ul className="nav nav-tabs justify-content-center" style={{marginBottom: "1em"}}>
-                    {this.props.children.map((c, i) =>
-                        <li className={ "nav-item" + (this.props.itemActive ? " active" : "") } 
-                            onClick={this.handleTabClick.bind(this)} 
+                    {this.props.children.map((child, i) =>
+                        <button className={"nav-item" + (this.props.itemActive ? " active" : "")}
                             key={i}
+                            onClick={this.handleTabClick.bind(this)}
+                            value={this.navBarId + i}
                         >
-                            {c}
-                        </li>
+                            {child}
+                        </button>
                     )}
                 </ul>
                 {this.populateChildren()}
