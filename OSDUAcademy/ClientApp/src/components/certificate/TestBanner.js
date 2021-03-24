1b6f7f9f38460f052dcbed6679b633f9894a2791
Alan Rostem <arostem@gmail.com>
2021-03-24 18:44:07 +0100

    Update TestBanner.js

--- OSDUAcademy/ClientApp/src/components/certificate/TestBanner.js
+++ OSDUAcademy/ClientApp/src/components/certificate/TestBanner.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {Container} from "reactstrap";
+import {Link} from "react-router-dom";
+import LearningService from "../../services/LearningService";
 
 /**
  * The component returns a banner showing the start of certification process and a go-back-to-course button.
@@ -13,7 +15,7 @@ export class TestBanner extends Component {
             <div className="certificate-banner">
                 <Container className="certificate-container">
                     <h1>Certificate</h1>
-                    <a href="/"><i className="fa fa-chevron-left" aria-hidden="true"/> Course </a>
+                    <Link to={"/learn/" + LearningService.getCurrentCourseRoute()}><i className="fa fa-chevron-left" aria-hidden="true"/> Course </Link>
                 </Container>
             </div>
         );
