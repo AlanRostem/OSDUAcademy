4d28dd263a8a78a266911ad425f9951bf1ecabb3
Nemanja Babic <nemanja0512@gmail.com>
2021-03-25 12:03:16 +0100

    profile page update

--- OSDUAcademy/ClientApp/src/sections/Profile.js
+++ OSDUAcademy/ClientApp/src/sections/Profile.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import {DefaultNavMenu} from "../components/navbar/DefaultNavMenu";
 import {Footer} from "../components/navbar/Footer";
 import {Container} from "reactstrap";
+import UserService from "../services/UserService"
+import {CategoryItem} from "../components/navbar/CategoryItem";
+import CourseRow from "../components/home/CourseRow";
+import {CategoryNavBar} from "../components/navbar/CategoryNavBar";
 
 /**
  * The component returns
@@ -15,22 +19,27 @@ export class Profile extends Component {
             <div>
                 <DefaultNavMenu/>
                 <Container className="profile-container">
-                    <div className="profile-info">
-                        <ul>
-                            <li>
-                                <span className="field">Name: </span>
-                                <span className="input">Jens Olsen</span>
-                            </li>
-                            <li>
-                                <span className="field">Email: </span>
-                                <span className="input">jens.olsen@gmail.com</span>
-                            </li>
-                            <li>
-                                <span className="field">Occupation: </span>
-                                <span className="input">Team Manager</span>
-                            </li>
-                        </ul>
-                    </div>
+                        
+                    <h5 className="profile-h"> User information </h5>
+                    <form className="profile-form">
+                        <div className="profile-info">
+                            <label>Name: </label>
+                            <input type="text" value={UserService.getUser().firstName + " " + UserService.getUser().lastName} readOnly/>
+                        </div>
+                        <div className="profile-info">
+                            <label>Email: </label>
+                            <input type="text" value={UserService.getUser().email} readOnly/>
+                        </div>
+                    </form>
+                    
+                    <br/>
+
+                    <h5 className="profile-h courses-h"> Courses </h5>
+                    <CategoryNavBar>
+                        <CategoryItem itemActive={true} componentToShow={<CourseRow searchByTrending={true}/>}>ACTIVE</CategoryItem>
+                        <CategoryItem itemActive={false} componentToShow={<CourseRow domainToSearchBy={"geology"}/>}>COMPLETED</CategoryItem>
+                    </CategoryNavBar>
+                    
                 </Container>
                 <Footer/>
             </div>
