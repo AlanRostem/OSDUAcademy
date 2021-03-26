7a951a3c2689a478ee1b0cfe94e3c0f7fa3960a0
Alan Rostem <arostem@gmail.com>
2021-03-26 09:34:39 +0100

    Tokens are now generated upon login

--- OSDUAcademy/Controllers/LoginController.cs
+++ OSDUAcademy/Controllers/LoginController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using MongoDB.Driver;
 using OSDUAcademy.DataTypes;
 
@@ -31,61 +33,50 @@ namespace OSDUAcademy.Controllers
             _userCollection = database.GetCollection<User>("users");
         }
 
-        [HttpPost]
-        public Dictionary<string, object> Post([FromBody] LoginData content)
-        {
-
-            var list = _userCollection
-                .Find(u => u.Email == content.Email)
-                .ToList();
-
-            var data = new Dictionary<string, object>
-            {
-                ["success"] = false
-            };
-
-            if (list.Count != 0)
-            {
-                var user = list[0];
-                if (user.Password == content.Password)
-                {
-                    user.Password = null;
-                    data["success"] = true;
-                    data["user"] = user;
-                }
-            }
-
-            return data;
-        }
-        
-        /*[AllowAnonymous]
+        [AllowAnonymous]
         [HttpPost]
         public IActionResult RequestToken([FromBody] LoginData request)
         {
-            if (request.Email == "jens.olsen@email.com" && request.Password == "jens123")
+            var userFields = UserFieldBuilder
+                .Include(u => u.Email)
+                .Include(u => u.Password)
+                .Include(u => u.FirstName)
+                .Include(u => u.LastName);
+            var list = _userCollection
+                .Find(u => u.Email == request.Email)
+                .Project<User>(userFields)
+                .ToList();
+
+            var data = new Dictionary<string, object>();
+            if (list.Count != 0)
             {
-                var claims = new[]
+                var user = list[0];
+                if (user.Password == request.Password)
                 {
-                    new Claim(ClaimTypes.Name, request.Email)
-                };
+                    user.Password = null;
+                    data["user"] = user;
+                    
+                    var claims = new[]
+                    {
+                        new Claim(ClaimTypes.Name, request.Email)
+                    };
 
-                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SecurityKey"]));
-                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this is a very secure key"));
+                    var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-                var token = new JwtSecurityToken(
-                    issuer: "https://demo.identityserver.io",
-                    audience: "http://localhost:5000",
-                    claims: claims,
-                    expires: DateTime.Now.AddMinutes(30),
-                    signingCredentials: creds);
+                    var token = new JwtSecurityToken(
+                        issuer: "https://demo.identityserver.io",
+                        audience: "http://localhost:5000",
+                        claims: claims,
+                        expires: DateTime.Now.AddMinutes(30),
+                        signingCredentials: credentials);
 
-                return Ok(new
-                {
-                    token = new JwtSecurityTokenHandler().WriteToken(token)
-                });
+                    data["token"] = new JwtSecurityTokenHandler().WriteToken(token);
+                    return Ok(data);
+                }
             }
 
             return BadRequest("Could not verify username and password");
-        }*/
+        }
     }
 }
