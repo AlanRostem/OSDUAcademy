08822997ca705735d3143fd74305270625ccecb1
Nemanja Babic <nemanja0512@gmail.com>
2021-03-26 16:59:27 +0100

    Update App.js

--- OSDUAcademy/ClientApp/src/App.js
+++ OSDUAcademy/ClientApp/src/App.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 import {Route} from 'react-router-dom';
 import {Home} from './sections/Home';
-import {FetchData} from './sections/FetchData';
-import {Counter} from './sections/Counter';
 import {Homeli} from './sections/Homeli';
 import './custom.css'
 import {Search} from "./sections/Search";
@@ -24,8 +22,6 @@ export default class App extends Component {
         return (
             <div>
                 <Route exact path='/' component={Home}/>
-                <Route path='/counter' component={Counter}/>
-                <Route path='/fetch-data' component={FetchData}/>
                 <Route path='/home-li' component={Homeli}/>
                 <Route exact path='/login-page' component={Login}/>
                 <Route path='/federated' component={FedLogin}/>
