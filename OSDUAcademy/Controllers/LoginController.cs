651c324772412667e6ada34ff16f5badc1c8a848
Alan Rostem <arostem@gmail.com>
2021-03-26 09:19:56 +0100

    Changed .NET verision to 5.0

--- OSDUAcademy/Controllers/LoginController.cs
+++ OSDUAcademy/Controllers/LoginController.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
 using OSDUAcademy.DataTypes;
@@ -53,5 +57,35 @@ namespace OSDUAcademy.Controllers
 
             return data;
         }
+        
+        /*[AllowAnonymous]
+        [HttpPost]
+        public IActionResult RequestToken([FromBody] LoginData request)
+        {
+            if (request.Email == "jens.olsen@email.com" && request.Password == "jens123")
+            {
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.Name, request.Email)
+                };
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SecurityKey"]));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: "https://demo.identityserver.io",
+                    audience: "http://localhost:5000",
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds);
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+
+            return BadRequest("Could not verify username and password");
+        }*/
     }
 }
